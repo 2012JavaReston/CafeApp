d97d334ee45b5144a3dd107d394a6bff853ca0b9
Lester Young <lestermyoung0789@gmail.com>
2020-12-29 17:58:39 -0500

    FoodController implemented

---
+++ src/main/java/com/revature/controllers/FoodControler.java
@@ -0,0 +1,69 @@
+package com.revature.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.dao.*
+import com.revature.models.Foods;
+
+public class FoodControler {
+	private static FoodDao foodDao = new FoodDaoImp(); 
+
+	public static void getFood(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		
+		if(req.getMethod().equals("GGET")) {
+			Foods food = null; 
+			resp.setContentType("application/json"); 
+			
+			String foodName = req.getParameter("name"); 
+			
+			food = foodDao.findFoodByName(foodName);
+			
+			ObjectMapper om = new ObjectMapper(); 
+			resp.getWriter().write(om.writeValueAsString(food));
+			
+			
+			
+		} else {
+			resp.setStatus(405);
+			System.out.println("No food found with that name");
+		}
+		
+	}
+	
+	
+	public static void postFood(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		
+		if(req.getMethod().equals("POST")) {
+			ObjectMapper om = new ObjectMapper(); 
+			Foods food = om.readValue(req.getReader(), com.revature.models.Foods.class); 
+			
+			foodDao.createFood(food); 
+			
+			resp.setStatus(201);
+			
+		} else {
+			resp.getStatus(405); 
+			System.out.println("Could not create new Food Item");
+		}
+		
+	}
+	
+	
+	public static void deleteFood(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		if(req.getMethod().equals("DELETE")) {
+			ObjectMapper om = new ObjectMapper(); 
+			Foods food = om.readValue(req.getReader(), com.revature.models.Foods.class); 
+			
+			foodDao.removeFood(food); 
+			
+		}else {
+			System.out.println("Was not successful delete");
+		}
+	}
+	
+}
